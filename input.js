c1437043d966715348059a956b9b1dc33720e5d1
joaoplucas2020-dotcom <joaoplucas2020@gmail.com>
2026-09-09 23:55:11 +0000

    exercicio

--- input.js
+++ input.js
@@ -1,1 +1,16 @@
-
+const escrever = require ("readline-sync")
+let nome= escrever.question ("Qual seu nome?") 
+var comidapreferida=escrever.question("Qual sua comida preferida ?")
+var idade = parseint(escrever.question ("Qual e sua  idade "))
+console.log(idade)
+if(idade >=25){
+    console.log ("pode entrar na festa , ebaaaaaaa")
+}else{   
+ console.log ("Volta pra casa , sorry")
+}
+if(idade>25 && comidapreferida== "Pizza"){
+    console.log("pode entar)
+}êlse if(idade >=25 && comidaPreferida=="lasanha"){
+}else{
+    console.log "volte amanhã porque hoje e pizzaaa"
+}
